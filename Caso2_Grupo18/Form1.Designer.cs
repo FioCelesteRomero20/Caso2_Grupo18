7dc28280c5bf84581168f7b045f6d505c496345a
FioCelesteRomero20 <rfiorella489@gmail.com>
2024-07-08 03:23:10 -0500

    Agregar archivos de proyecto.

---
+++ Caso2_Grupo18/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Caso2_Grupo18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvRegistroVideojuego = new System.Windows.Forms.DataGridView();
+            this.buttonRegistrar = new System.Windows.Forms.Button();
+            this.rbtnPerdió = new System.Windows.Forms.RadioButton();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.RButtGane = new System.Windows.Forms.RadioButton();
+            this.textNewEliminar = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblPuntosTotales = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonQuitar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistroVideojuego)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonQuitar);
+            this.groupBox1.Controls.Add(this.dgvRegistroVideojuego);
+            this.groupBox1.Controls.Add(this.buttonRegistrar);
+            this.groupBox1.Controls.Add(this.rbtnPerdió);
+            this.groupBox1.Controls.Add(this.buttonSalir);
+            this.groupBox1.Controls.Add(this.RButtGane);
+            this.groupBox1.Controls.Add(this.textNewEliminar);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.lblPuntosTotales);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(32, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(532, 469);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de videojuegos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // dgvRegistroVideojuego
+            // 
+            this.dgvRegistroVideojuego.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvRegistroVideojuego.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistroVideojuego.Location = new System.Drawing.Point(17, 257);
+            this.dgvRegistroVideojuego.Name = "dgvRegistroVideojuego";
+            this.dgvRegistroVideojuego.Size = new System.Drawing.Size(240, 150);
+            this.dgvRegistroVideojuego.TabIndex = 13;
+            // 
+            // buttonRegistrar
+            // 
+            this.buttonRegistrar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.buttonRegistrar.Location = new System.Drawing.Point(217, 110);
+            this.buttonRegistrar.Name = "buttonRegistrar";
+            this.buttonRegistrar.Size = new System.Drawing.Size(83, 27);
+            this.buttonRegistrar.TabIndex = 3;
+            this.buttonRegistrar.Text = "Registrar";
+            this.buttonRegistrar.UseVisualStyleBackColor = false;
+            this.buttonRegistrar.Click += new System.EventHandler(this.buttonRegistrar_Click);
+            // 
+            // rbtnPerdió
+            // 
+            this.rbtnPerdió.AutoSize = true;
+            this.rbtnPerdió.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rbtnPerdió.Location = new System.Drawing.Point(419, 73);
+            this.rbtnPerdió.Name = "rbtnPerdió";
+            this.rbtnPerdió.Size = new System.Drawing.Size(75, 17);
+            this.rbtnPerdió.TabIndex = 6;
+            this.rbtnPerdió.TabStop = true;
+            this.rbtnPerdió.Text = "Perdí (-20)";
+            this.rbtnPerdió.UseVisualStyleBackColor = true;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.buttonSalir.Location = new System.Drawing.Point(419, 193);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(83, 27);
+            this.buttonSalir.TabIndex = 10;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = false;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // RButtGane
+            // 
+            this.RButtGane.AutoSize = true;
+            this.RButtGane.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RButtGane.Location = new System.Drawing.Point(419, 28);
+            this.RButtGane.Name = "RButtGane";
+            this.RButtGane.Size = new System.Drawing.Size(84, 17);
+            this.RButtGane.TabIndex = 2;
+            this.RButtGane.TabStop = true;
+            this.RButtGane.Text = "Gané (+100)";
+            this.RButtGane.UseVisualStyleBackColor = true;
+            this.RButtGane.CheckedChanged += new System.EventHandler(this.RButtGane_CheckedChanged);
+            // 
+            // textNewEliminar
+            // 
+            this.textNewEliminar.Location = new System.Drawing.Point(252, 154);
+            this.textNewEliminar.Name = "textNewEliminar";
+            this.textNewEliminar.Size = new System.Drawing.Size(250, 20);
+            this.textNewEliminar.TabIndex = 12;
+            this.textNewEliminar.TextChanged += new System.EventHandler(this.textNewEliminar_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // lblPuntosTotales
+            // 
+            this.lblPuntosTotales.AutoSize = true;
+            this.lblPuntosTotales.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPuntosTotales.Location = new System.Drawing.Point(26, 30);
+            this.lblPuntosTotales.Name = "lblPuntosTotales";
+            this.lblPuntosTotales.Size = new System.Drawing.Size(126, 13);
+            this.lblPuntosTotales.TabIndex = 5;
+            this.lblPuntosTotales.Text = "Nombre del Videojuego : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(26, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ingrese Videojuego a Eliminar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(26, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Fecha :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Location = new System.Drawing.Point(174, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // buttonQuitar
+            // 
+            this.buttonQuitar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.buttonQuitar.Location = new System.Drawing.Point(278, 193);
+            this.buttonQuitar.Name = "buttonQuitar";
+            this.buttonQuitar.Size = new System.Drawing.Size(83, 27);
+            this.buttonQuitar.TabIndex = 14;
+            this.buttonQuitar.Text = "Quitar";
+            this.buttonQuitar.UseVisualStyleBackColor = false;
+            this.buttonQuitar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 531);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistroVideojuego)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvRegistroVideojuego;
+        private System.Windows.Forms.Button buttonRegistrar;
+        private System.Windows.Forms.RadioButton rbtnPerdió;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.RadioButton RButtGane;
+        private System.Windows.Forms.TextBox textNewEliminar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblPuntosTotales;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonQuitar;
+    }
+}
+
---
+++ Caso2_Grupo18/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caso2_Grupo18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DatosACargar();
+        }
+
+        public class EF_Grupo18
+        {
+            //creamos sus atributos...
+            public string Nombre_Grupo18 { get; set; }
+            public string Fecha_Grupo18 { get; set; }
+            public bool G_o_P_Grupo18 { get; set; }
+            public int Puntos_Grupo18 { get; set; }
+            public string Resultado_Grupo18 => G_o_P_Grupo18 ? "Ganado" : "Perdido";
+            //Asignamos atributos con variable llamando al constructor...
+            public EF_Grupo18(string nombre_Grupo18, string sfecha_Grupo18, bool g_o_p_Grupo18, int puntos_Grupo18)
+            {
+                Nombre_Grupo18 = nombre_Grupo18;
+                Fecha_Grupo18 = sfecha_Grupo18;
+                G_o_P_Grupo18 = g_o_p_Grupo18;
+                Puntos_Grupo18 = puntos_Grupo18;
+            }
+
+            public override string ToString()
+            {
+                return $"{Nombre_Grupo18},{Fecha_Grupo18},{G_o_P_Grupo18},{Puntos_Grupo18}";
+            }
+
+        }
+
+        private const string FiloPath = "base_de_informacion.txt";
+        private int totalPuntos_Grupo18;
+
+        private void GuardarRegistro(EF_Grupo18 eF_Grupo18)
+        {
+            try
+            {
+                using (StreamWriter esc_Grupo18 = new StreamWriter(FiloPath, true))
+                {
+                    esc_Grupo18.WriteLine(eF_Grupo18.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Estan en error los datos registrados " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DatosACargar()
+        {
+            if (File.Exists(FiloPath))
+            {
+                try
+                {
+                    string[] lin_Grupo18 = File.ReadAllLines(FiloPath);
+                    // Para que podamos leer cada dato del txt es necesario recorrer una linea ...
+                    foreach (string l_Grupo18 in lin_Grupo18)
+                    {
+                        string[] p_Grupo18 = l_Grupo18.Split('.');
+                        if (p_Grupo18.Length == 4)
+                        {
+                            string nombreJuego_Grupo18 = p_Grupo18[0];
+                            string sfecha_Grupo18 = p_Grupo18[1];
+                            bool ganado_Grupo18 = bool.Parse(p_Grupo18[2]);
+                            int puntos_Grupo18 = int.Parse(p_Grupo18[3]);
+
+                            //Para recuperar los datos, creamos un objeto de la clase
+                            EF_Grupo18 eF_Grupo18 = new EF_Grupo18(nombreJuego_Grupo18, sfecha_Grupo18, ganado_Grupo18, puntos_Grupo18);
+                            // Vemos los datos de DateGriedView
+                            dgvRegistroVideojuego.Rows.Add(eF_Grupo18.Nombre_Grupo18, eF_Grupo18.Fecha_Grupo18, eF_Grupo18.Resultado_Grupo18, eF_Grupo18.Puntos_Grupo18);
+                            // Cargamos el total de puntos
+                            totalPuntos_Grupo18 += puntos_Grupo18;
+                        }
+                    }
+                    // Vemos los puntos totales...
+                    lblPuntosTotales.Text = "Total de Puntos: " + totalPuntos_Grupo18.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Se detectó un error en los datos cargados, " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void EliminarDatos(string nombreJuego_Grupo18)
+        {
+            try
+            {
+                string[] l_Grupo18 = File.ReadAllLines(FiloPath);
+                using (StreamWriter registro = new StreamWriter(FiloPath, false))
+                {
+                    foreach (string li_Grupo18 in l_Grupo18)
+                    {
+                        string[] s_partes_Grupo18 = li_Grupo18.Split('.');
+                        if (s_partes_Grupo18.Length == 4 && s_partes_Grupo18[0] != nombreJuego_Grupo18)
+                        {
+                            registro.WriteLine(li_Grupo18);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se detectó un error en los datos para eliminar " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonRegistrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombreJuego_Grupo18 = textBox2.Text.Trim();
+                bool ganado_Grupo18 = RButtGane.Checked;
+                string sfecha_Grupo18 = DateTime.Now.ToString("dd/MM/yyyy");
+
+                //No permitir el ingreso de valores vacios
+                if (string.IsNullOrWhiteSpace(nombreJuego_Grupo18))
+                {
+                    throw new ArgumentException();
+                }
+                //Almacenamos los puntos
+                int puntos_Grupo18;
+                if (ganado_Grupo18 == true)
+                {
+                    puntos_Grupo18 = 100;
+                }
+                else
+                {
+                    puntos_Grupo18 = -20;
+                }
+                totalPuntos_Grupo18 += puntos_Grupo18;
+
+                //Ingresamos los parametros correspondientes...
+                EF_Grupo18 eF_Grupo18 = new EF_Grupo18(nombreJuego_Grupo18, sfecha_Grupo18, ganado_Grupo18, puntos_Grupo18);
+
+                // Guardamos los datos en el texto que elegimos ...
+                GuardarRegistro(eF_Grupo18);
+
+                //Registramos los datos en DateGriedView
+                dgvRegistroVideojuego.Rows.Add(eF_Grupo18.Nombre_Grupo18, eF_Grupo18.Fecha_Grupo18, eF_Grupo18.Resultado_Grupo18, eF_Grupo18.Puntos_Grupo18);
+
+                //Imprimimos con los puntos acumulados
+                lblPuntosTotales.Text = "Total de Puntos: " + totalPuntos_Grupo18.ToString();
+
+                //Limpiamos
+                textBox2.Text = "";
+                RButtGane.Checked = true;
+
+            }
+            catch 
+            { 
+               MessageBox.Show("Ingrese el nombre del videojuego", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+        }
+
+
+        private void RButtGane_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombreJuego_Grupo18 = textNewEliminar.Text.Trim();
+                if (string.IsNullOrWhiteSpace(juegoEliminar_Grupo18))
+                {
+                    throw new ArgumentException();
+                }
+                bool juegoEncontrado_Grupo18 = false;
+                DataGridView filaEliminar_Grupo18 = null;
+
+                //Observamos cada fila
+                foreach (DataGridView fila in dgvRegistroVideojuego.Rows)
+                {
+                    string nombreJuego_Grupo18 = fila.Cells["Juego"].Value.ToString();
+                    //Comparamos
+                    if (nombreJuego_Grupo18.Equals(juegoEliminar_Grupo18, StringComparison.OrdinalIgnoreCase))
+                    {
+                        filaEliminar_Grupo18 = fila;
+                        juegoEncontrado_Grupo18 = true;
+                        break;
+                    }
+                    //Observamos la fila que se debe eliminar
+
+                }
+                if (juegoEncontrado_Grupo18)
+                {
+                    //Restamos los puntos
+                    int puntos_Grupo18 = Convert.ToInt32(filaEliminar_Grupo18.CellS["Puntos"].Value);
+                    totalPuntos_Grupo18 = puntos_Grupo18;
+
+                    //Eliminamos la fila
+                    dgvRegistroVideojuego.Rows.Remove(filaEliminar_Grupo18);
+
+                    //Funcion eliminar
+                    EliminarDatos(juegoEliminar_Grupo18);
+
+                    //Actualizamos los puntos
+                    lblPuntosTotales.Text = "Total de Puntos: " + totalPuntos_Grupo18.ToString();
+
+                    MessageBox.Show("El juego se elimino correctamente.", "Eliminar juego", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("El juego no se encontró en la lista.", "Eliminar juego", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                textNewEliminar.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe ingresar el nombre del juego a eliminar.", "Eliminar juego", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textNewEliminar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
